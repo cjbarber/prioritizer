7a56b49c3a775aa20cea37a2947bb6a4ebcd0c36
Chris Barber <c.barber80@gmail.com>
2013-09-03 12:35:24 -0700

    Removed duplicate setting of random, already redone in item_choose.js

--- client/views/items/items_vs.js
+++ client/views/items/items_vs.js
@@ -1,12 +1,6 @@
 Template.itemsVs.helpers({
   items: function() {
     var items_to_vs = Items.find({}, {sort: {times_compared: 1, random: 1}, limit: 2});
-    // Easier way to prevent most repeat comparisons. Just re-randomize them.
-    items_to_vs.forEach(function (item) {
-      Items.update(item._id, {
-        random: Math.random()
-      });
-    });
     return items_to_vs;
   }
 });
